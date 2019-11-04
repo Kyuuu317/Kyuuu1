8de7503d27fc4fd4f0e10b97965c7e46b2417b67
Kyuuu317 <57291866+Kyuuu317@users.noreply.github.com>
2019-11-03 23:59:31 -0500

    Add files via upload

---
+++ KeyuLin-3618.R
@@ -0,0 +1,10 @@
+rownames = c("Mummy","Zombie")
+colnames = c("hard","Cottom","taffy","jelly","Loillipops")
+dimnames = list(rownames,colnames)
+M1<-matrix(c(1,2,3,4,5,6,7,8,9,10,11,12),nrow = 2,ncol = 6,dimnames = dimnames)
+print(M1)
+
+
+
+
+
